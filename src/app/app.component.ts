1e65e00f9c0eb8cbb1e504d6b8af953761be879e
Abdul Basit <bashialy@gmail.com>
2019-07-10 19:06:02 +0500

    Holder Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, forwardRef, Input, OnInit,ViewEncapsulation } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import '../assets/app-email-element';
+import '../assets/app-phone-element';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +9,17 @@ import '../assets/app-email-element';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  email1 = '';
-  email2 = '';
-  constructor() { }
+  name = '';
+  options = { email: '', type:'', isRequired: false, isPrimary: false, label: 'This is label', types: [{name: 'Home', value: 'home'}, {name: 'Personal', value: 'personal'}, {name: 'Other', value: 'other'}],
+   isValid: -1
+  };
 
-  show() {
-    console.log('this', this.email1, this.email2);
+    options1 = { phone: '', type:'', isRequired: false, isCanadian: false, label: 'This is label', types: [{name: 'Home', value: 'home'}, {name: 'Personal', value: 'personal'}, {name: 'Other', value: 'other'}],
+   isValid: -1
+  };
+    constructor() {}
+  getValue(form) {
+      console.log("Outside Component", this.name, this.options)
   }
 
 }
